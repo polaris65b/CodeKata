d65af298dea2b6b48d9afb067d17cad3d37ccf5c
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-07 10:06:52 +0900

    Create p11.java

---
+++ Level1/p11.java
@@ -0,0 +1,14 @@
+package CodeKata.Level1;
+
+class p11 {
+    public String solution(int num) {
+        String answer = "";
+
+        if(num % 2 == 0)
+            answer = "Even";
+        else
+            answer = "Odd";
+
+        return answer;
+    }
+}

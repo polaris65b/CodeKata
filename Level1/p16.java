3aa53fe79506d08c6f77bc1cd8178519ad44c146
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-08 09:52:04 +0900

    Create p16.java

---
+++ Level1/p16.java
@@ -0,0 +1,19 @@
+package CodeKata.Level1;
+
+import java.util.Arrays;
+
+public class p16 {
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long) x * (i + 1);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        p16 instance = new p16();
+        long[] result = instance.solution(2, 5);
+        System.out.println(Arrays.toString(result));
+    }
+}

998390a98b66834121f1a1ed613e14b18d27a97e
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-01-09 10:21:41 +0900

    p18

---
+++ Level1/p18.java
@@ -0,0 +1,14 @@
+package CodeKata.Level1;
+
+public class p18 {
+    public int solution(String s){
+        int answer = Integer.parseInt(s);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        p18 sol = new p18();
+        int result = sol.solution("1234");
+        System.out.println(result);
+    }
+}
---
+++ test.java
@@ -0,0 +1,14 @@
+package CodeKata;
+
+public class test{
+    public static void main(String[] args){
+        String str1 = "123";
+        String str2 = "-123";
+
+        int intValue = Integer.parseInt(str1);
+        System.out.println(intValue); // 123
+
+        intValue = Integer.parseInt(str2);
+        System.out.println(intValue); // -123
+    }
+}

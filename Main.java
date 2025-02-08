79e61faea85ee7c418c545643d0161bd8731cfea
polaris65b <polaris65b@gmail.com>
2025-02-08 19:15:19 +0900

    P23

---
+++ Main.java
@@ -0,0 +1,13 @@
+package CodeKata;
+
+import CodeKata.Level1.*;
+//import CodeKata.Level2.*;
+
+public class Main {
+    public static void main(String[] args) {
+        P23 sol = new P23();
+        System.out.println(sol.solution(6));
+        System.out.println(sol.solution(16));
+        System.out.println(sol.solution(626331));
+    }
+}

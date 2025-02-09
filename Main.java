d85ea49aaf4caa9e7f6a22c80f8a73a9957123e8
polaris65b <42924432+polaris65b@users.noreply.github.com>
2025-02-09 16:43:51 +0900

    P27

--- Main.java
+++ Main.java
@@ -4,14 +4,7 @@ import CodeKata.Level1.P21_30.*;
 
 public class Main {
     public static void main(String[] args) {
-        P26 sol = new P26();
-
-        int[] absolutes = {4, 7, 12};
-        boolean[] signs = {true, false, true};
-        System.out.println(sol.solution(absolutes, signs));
-
-        absolutes = new int[]{1, 2, 3};
-        signs = new boolean[]{false, false, true};
-        System.out.println(sol.solution(absolutes, signs));
+        P27 sol = new P27();
+        System.out.println(sol.solution("01033334444"));
     }
 }
